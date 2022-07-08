11b0de400f66625b8990085d4d16f180d38b9905
IgnatiusGL <ignatiusgl@foxmail.com>
2022-07-09 07:26:33 +0800

    refine: update the StringUtils test case

--- src/test/java/test/string/StringUtilsTest.java
+++ src/test/java/test/string/StringUtilsTest.java
@@ -18,12 +18,17 @@ public class StringUtilsTest {
                 "asd12*12312asd",
                 "asd*asd",
                 "asd12*1231-2asd",
-                "sdasd-*+wqd"
+                "sdasd-*+wqd",
         };
         for (int i = 0; i < testString.length; i++) {
             testString[i] = StringUtils.replaceAllToOne(testString[i], new String[]{"*","+","-"}, "");
         }
-        System.out.println("StringUtils-replaceAllToOne:" + Arrays.asList(testString));
+        assert testString[0].equals("testasd");
+        assert testString[1].equals("asd1212312asd");
+        assert testString[2].equals("asdasd");
+        assert testString[3].equals("asd1212312asd");
+        assert testString[4].equals("sdasdwqd");
+        System.out.println("StringUtils-replaceAllToOne: pass");
 
         System.out.println("----------------------------------------------------");
 
@@ -32,12 +37,18 @@ public class StringUtilsTest {
                 "big-data",
                 "background-icon",
                 "button-login",
-                "submit-record"
+                "submit-record",
         };
         for (int i = 0; i < testString.length; i++) {
             testString[i] = StringUtils.toHumpString(testString[i], "-", true);
         }
-        System.out.println("StringUtils-toHumpString1:" + Arrays.asList(testString));
+        assert testString[0].equals("TestAsd");
+        assert testString[1].equals("BigData");
+        assert testString[2].equals("BackgroundIcon");
+        assert testString[3].equals("ButtonLogin");
+        assert testString[4].equals("SubmitRecord");
+        System.out.println("StringUtils-toHumpString1: pass");
+
         testString = new String[]{
                 "test_asd",
                 "big_data",
@@ -45,12 +56,19 @@ public class StringUtilsTest {
                 "button_login",
                 "submit_record",
                 "submit_record_1",
-                "submit_record1"
+                "submit_record1",
         };
         for (int i = 0; i < testString.length; i++) {
             testString[i] = StringUtils.toHumpString(testString[i], "_", false);
         }
-        System.out.println("StringUtils-toHumpString2:" + Arrays.asList(testString));
+        assert testString[0].equals("testAsd");
+        assert testString[1].equals("bigData");
+        assert testString[2].equals("backgroundIcon");
+        assert testString[3].equals("buttonLogin");
+        assert testString[4].equals("submitRecord");
+        assert testString[5].equals("submitRecord1");
+        assert testString[6].equals("submitRecord1");
+        System.out.println("StringUtils-toHumpString2: pass");
 
         System.out.println("----------------------------------------------------");
 
@@ -61,12 +79,18 @@ public class StringUtilsTest {
                 "buttonLogin",
                 "SubmitRecord",
                 "submitRecord1",
-                "SubmitRecord1"
+                "SubmitRecord1",
         };
-
         for (int i = 0; i < testString.length; i++) {
             testString[i] = StringUtils.toSeparatorString(testString[i], "_");
         }
-        System.out.println("StringUtils-toSeparatorString:" + Arrays.asList(testString));
+        assert testString[0].equals("test_asd");
+        assert testString[1].equals("big_data");
+        assert testString[2].equals("background_icon");
+        assert testString[3].equals("button_login");
+        assert testString[4].equals("submit_record");
+        assert testString[5].equals("submit_record_1");
+        assert testString[6].equals("submit_record_1");
+        System.out.println("StringUtils-toSeparatorString: pass");
     }
 }
